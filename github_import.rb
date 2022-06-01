ca18f003eba0a170455009006efc3a5ff95aab97
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-06-01 17:19:37 -0400

    Use super-conservative sleep time between each story creation

--- github_import.rb
+++ github_import.rb
@@ -13,7 +13,7 @@ class GithubImport
     stories.each do |story|
       puts "Creating issue for #{story.title}"
       client.create_issue(ENV["GITHUB_REPO"], story.title, story.body, labels: story.labels)
-      sleep 10
+      sleep 20
     end
   end
 
